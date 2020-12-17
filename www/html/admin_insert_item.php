097ed97a544fcac9bfc32c65c79aad4717e1ebe8
川村竜也 <kawamuraryuya@kawamuratatsuyanoMacBook-Air.local>
2020-12-17 12:46:09 +0900

    CSRF対策 #7

--- www/html/admin_insert_item.php
+++ www/html/admin_insert_item.php
@@ -24,11 +24,16 @@ $status = get_post('status');
 $stock = get_post('stock');
 
 $image = get_file('image');
+$token = get_post('token');
 
-if(regist_item($db, $name, $price, $stock, $status, $image)){
-  set_message('商品を登録しました。');
-}else {
-  set_error('商品の登録に失敗しました。');
+if(is_valid_csrf_token($token)===true){
+  if(regist_item($db, $name, $price, $stock, $status, $image)){
+    set_message('商品を登録しました。');
+  }else {
+    set_error('商品の登録に失敗しました。');
+  }
+} else {
+  set_error('不正な操作が行われました');
 }
 
 
--- www/html/finish.php
+++ www/html/finish.php
@@ -15,10 +15,14 @@ $db = get_db_connect();
 $user = get_login_user($db);
 
 $carts = get_user_carts($db, $user['user_id']);
-
-if(purchase_carts($db, $carts) === false){
-  set_error('商品が購入できませんでした。');
-  redirect_to(CART_URL);
+$token = get_post('token');
+if(is_valid_csrf_token($token)===true){
+  if(purchase_carts($db, $carts) === false){
+    set_error('商品が購入できませんでした。');
+    redirect_to(CART_URL);
+  }
+} else {
+  set_error('不正な操作が行われました');
 } 
 
 $total_price = sum_carts($carts);
